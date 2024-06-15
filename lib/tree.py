7fdc0240b7c698b7836b997af14c8cd496f9c47b
Justin Onyango <ojustin446@gmail.com>
2024-06-15 13:42:32 +0300

    Complete tree traversal

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,16 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    return self._get_element_by_id(self.root, id)
+
+  def _get_element_by_id(self, node, id):
+    if node.get('id') == id:
+      return node
+
+
+    for child in node.get('children', []):
+      result = self._get_element_by_id(child, id)
+      if result:
+        return result
+
+    return None
